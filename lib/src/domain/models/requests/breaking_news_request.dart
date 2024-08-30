42b55c32475309002c02851e275778362fc1d702
Nhan Nguyen <nguyendinhnhanqn@gmail.com>
2024-08-30 14:27:40 +0700

    add example url

--- lib/src/domain/models/requests/breaking_news_request.dart
+++ lib/src/domain/models/requests/breaking_news_request.dart
@@ -2,6 +2,7 @@ import '../../../utils/constants/nums.dart';
 import '../../../utils/constants/strings.dart';
 
 // https://newsapi.org/v2/top-headlines?country=us&apiKey=YOUR_API_KEY
+// https://newsapi.org/v2/top-headlines?apiKey=058dc1d8f0564d6387c3f1889ebbb977&sources=bbc-news%2C+abc-news&page=1&pageSize=20
 
 class BreakingNewsRequest {
   final String apiKey;
