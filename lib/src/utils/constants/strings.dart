6f4e8f052ec5d3104333cf152f997e6feecf62aa
Nhan Nguyen <nguyendinhnhanqn@gmail.com>
2024-09-23 11:00:46 +0700

    update changes

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -1,6 +1,7 @@
 // App
 const String appTitle = 'Flutter Clean Architecture';
 
+// Should be put at .env, however this is for easily reference
 // Networking and APIs
 const String baseUrl = 'https://newsapi.org/v2';
 const String defaultApiKey = '058dc1d8f0564d6387c3f1889ebbb977';
