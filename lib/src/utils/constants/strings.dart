6133a880d29dd1f225e25c39cc9248f3e70b51a0
Nhan Nguyen <nguyendinhnhanqn@gmail.com>
2024-10-09 15:22:04 +0700

    update comments

--- lib/src/utils/constants/strings.dart
+++ lib/src/utils/constants/strings.dart
@@ -1,10 +1,11 @@
 // App
 const String appTitle = 'Flutter Clean Architecture';
 
-// Should be put at .env, however this is for easily reference
 // Networking and APIs
-const String baseUrl = 'https://newsapi.org/v2';
-const String defaultApiKey = '058dc1d8f0564d6387c3f1889ebbb977';
+const String baseUrl =
+    'https://newsapi.org/v2'; // Should be put at .env, however this is for easy reference
+const String defaultApiKey =
+    '058dc1d8f0564d6387c3f1889ebbb977'; // Should be put at .env, however this is for easy reference
 const String defaultSource = 'crypto-coins-news';
 
 // Storage and Databases
